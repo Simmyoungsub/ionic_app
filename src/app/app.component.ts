40ff6bac37f208447e59229b43661591525c5184
심명섭 <myungsubsim@idatabank.com>
2017-09-21 17:27:35 +0900

    사이드&푸터바 수정중

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,16 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+export interface PageInterface{
+  title : string;
+  name : string;
+  component : any;
+  icon : string;
+  index?: number;
+  tabName?: string;
+  tabComponent?: any;
+}
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -14,7 +24,12 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage:any = TabsPage;
   //사이드 메뉴 목록
-  pages: Array<{title:string,component:any}>;
+  // pages: Array<{title:string,component:any}>;
+  pages : PageInterface[] = [
+    {title : 'Home', name:'HomePage',component: HomePage, tabComponent:HomePage, index:0,icon:'home'},
+    {title : 'List', name:'ListPage', component: ListPage, tabComponent: ListPage, icon: 'list' },
+    // {title : 'Tabs Page', component: TabsPage}
+  ]
 
   constructor(private app:App,platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -24,17 +39,28 @@ export class MyApp {
       splashScreen.hide();
     });
 
-    this.pages = [
-      {title : 'Home', component: HomePage},
-      {title : 'List', component: ListPage},
-      {title : 'Tabs Page', component: TabsPage}
-    ]
+    // this.pages = [
+    //   {title : 'Home', component: HomePage},
+    //   {title : 'List', component: ListPage},
+    //   {title : 'Tabs Page', component: TabsPage}
+    // ]
   }
 
   //페이지 이동
-  openPage(page){
+  openPage(page: PageInterface){
+    let params = {};
+
+    if(page.index){
+      params = {tabindex:page.index};
+    }
+
+    if (this.nav.getActiveChildNavs().length && page.index != undefined) {
+      this.nav.getActiveChildNavs()[0].select(page.index);
+    } else {
+      this.nav.setRoot(page.component, params);
+    }
     //this.nav.setRoot(page.component);
-    this.app.getRootNav().setRoot(page.component);
+    // this.app.getRootNav().setRoot(page.component);
     // this.navCtrl.setRoot(page.component);
   }
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NavController, NavParams } from 'ionic-angular';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
@@ -9,11 +9,12 @@ import { HomePage } from '../home/home';
 })
 export class TabsPage {
 
+  myIndex: number;
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
 
-  constructor() {
-
+  constructor(navParams: NavParams) {
+    this.myIndex = navParams.data.tabIndex || 0;
   }
 }
